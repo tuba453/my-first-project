d3c5afacaa76f1292f669316a47dc8835038001d
unknown <tubatoker4789@gmail.com>
2025-10-24 10:41:07 +0300

    Veritabanında password_hash uzunluğunu artır

--- models.py
+++ models.py
@@ -11,7 +11,8 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(150), nullable=False)
+    # scrypt hash'leri 150 karakterden uzun olabildiği için bu alanı genişletiyoruz.
+    password_hash = db.Column(db.String(256), nullable=False)
     notes_deleted_count = db.Column(db.Integer, default=0) # YENİ: Silinen not sayacı
     avatar = db.Column(db.String(100), nullable=True) # YENİ: Kullanıcı avatarı dosya adı
 
